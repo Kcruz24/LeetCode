12b5721644b2d22e518515118ba41b627ac318c5
Kevin O. Cruz <kocruz.24@gmail.com>
2022-09-05 18:31:38 -0400

    Update "1029. Two City Scheduling" - Solution

--- Bloomberg MFA Questions/1029. Two City Scheduling/two-city-scheduling.py
+++ Bloomberg MFA Questions/1029. Two City Scheduling/two-city-scheduling.py
@@ -1,62 +1,25 @@
 from typing import List
 
 
-class Solution3:
-    # O(NLog(N)) time | O(N) space
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        refund = []
-        N = len(costs) // 2
-        minCost = 0
-
-        for A, B in costs:
-            refund.append(B - A)
-            minCost += A
-
-        refund.sort()
-
-        for i in range(N):
-            minCost += refund[i]
-
-        return minCost
-
-class Solution2:
-
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # first assume that everyone is going to city A
-        # then identify N people to go to city B
-        # according to how much they can help save when
-        # changing destination
-        # to city B
-        N = len(costs)//2
-        total_cost_A = 0
-        d = []
-        #
-        for i in range(2*N):
-            total_cost_A += costs[i][0]
-            d.append(costs[i][0]-costs[i][1])
-        # sort the difference (from large to small)
-        d.sort(reverse=True)
-
-        return(total_cost_A - sum(d[0:N]))
-
-
 class Solution:
-    # O(NLog(N)) time | O(1) space
+    # O(NLog(N)) Time | O(1) Space
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        # Sort by a gain which company has
-        # by sending a person to city A and not to city B
+
         costs.sort(key=lambda x: x[0] - x[1])
 
+        half = len(costs) // 2
 
-        total = 0
-        n = len(costs) // 2
-        # To optimize the company expenses,
-        # send the first n persons to the city A
-        # and the others to the city B
-        for i in range(n):
-            total += costs[i][0] + costs[i + n][1]
-            print(f'{costs[i][0]} + {costs[i + n][1]} = {total}')
-        return total
+        min_cost = 0
+        city_a = 0
+        city_b = 0
+
+        for i in range(half):
+            city_a = costs[i][0]
+            city_b = costs[i + half][1]
+
+        min_cost += city_a + city_b
+
+        return min_cost
 
 
 if __name__ == '__main__':
